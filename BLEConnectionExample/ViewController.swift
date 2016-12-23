bf322b7efc9566aef695ef52b0ecd370fae6dcaa
門脇裕 <gates1de@yus-macbook-pro.local>
2016-12-23 13:17:00 +0900

    Initial Commit

---
+++ BLEConnectionExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLEConnectionExample
+//
+//  Created by Yu Kadowaki on 12/23/16.
+//  Copyright © 2016 gates1de. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
